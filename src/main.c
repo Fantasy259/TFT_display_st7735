116675c70abb7d48ffcd53c741363eb9b22a55a7
Michael Halbfas <Michael.halbfas@wittmann-group.com>
2025-10-31 21:10:09 +0100

    update

--- src/main.c
+++ src/main.c
@@ -52,10 +52,9 @@ static void start_lvgl_tick(void)
 }
 
 /* Send short command to the LCD. This function shall wait until the transaction finishes. */
-
 int32_t my_lcd_send_cmd(lv_display_t *disp, const uint8_t *cmd, size_t cmd_size, const uint8_t *param, size_t param_size)
 {
-    ESP_LOGI("ST7735", "send_CMD cmd=0x%02X, size cmd =%d , param = 0x%02x, size Param %d", cmd ? cmd[0] : 0, (int) cmd_size, param ? param[0] : 0, (int)param_size);
+ //   ESP_LOGI("ST7735", "send_CMD cmd=0x%02X, size cmd =%d , param = 0x%02x, size Param %d", cmd ? cmd[0] : 0, (int) cmd_size, param ? param[0] : 0, (int)param_size);
     esp_err_t ret;
 
     // 1. Command senden (DC = 0)
@@ -85,11 +84,11 @@ int32_t my_lcd_send_cmd(lv_display_t *disp, const uint8_t *cmd, size_t cmd_size,
 /* Send large array of pixel data to the LCD. If necessary, this function has to do the byte-swapping. This function can do the transfer in the background. */
 int32_t my_lcd_send_color(lv_display_t *disp, const uint8_t *cmd, size_t cmd_size, uint8_t *param, size_t param_size)
 {
- //    ESP_LOGI(TAG , "xCall2");
-    ESP_LOGI("ST7735", "send_color: cmd=0x%02X, size=%d", cmd ? cmd[0] : 0, (int)param_size);
+
+//    ESP_LOGI("ST7735", "send_color: cmd=0x%02X, size=%d", cmd ? cmd[0] : 0, (int)param_size);
          esp_err_t ret;
 
-
+    // RGB565 in BRG565 Farben tauschen 
     for(size_t i = 0; i < param_size; i += 2)
     {
         uint16_t pixel = (param[i] << 8) | param[i+1]; // RGB565
@@ -103,7 +102,6 @@ int32_t my_lcd_send_color(lv_display_t *disp, const uint8_t *cmd, size_t cmd_siz
         param[i]   = (bgr >> 8) & 0xFF;
         param[i+1] = bgr & 0xFF;
     }
-
 
     // 1. Command schicken (z. B. 0x2C)
     if (cmd != NULL && cmd_size > 0) {
@@ -129,64 +127,6 @@ int32_t my_lcd_send_color(lv_display_t *disp, const uint8_t *cmd, size_t cmd_siz
 
     lv_display_flush_ready(disp);
     return 0; // Erfolg
-}
-
-
-int32_t myComando( const uint8_t *cmd, size_t cmd_size, const uint8_t *param, size_t param_size)
-{
-    ESP_LOGI("ST7735", "comando-> cmd=0x%02X, size cmd =%d , param = 0x%02x, size Param %d", cmd ? cmd[0] : 0, (int) cmd_size, param ? param[0] : 0, (int)param_size);
-    esp_err_t ret;
-
-    // 1. Command senden (DC = 0)
-    gpio_set_level(TFT_DC, 0);
-    spi_transaction_t t_cmd = {
-        .length = cmd_size * 8,
-        .tx_buffer = cmd,
-    };
-    ret = spi_device_polling_transmit(spi, &t_cmd);
-  
-    if (ret != ESP_OK) return -1;
-
-    // 2. Parameterdaten senden (falls vorhanden, DC = 1)
-    if (param != NULL && param_size > 0) {
-        gpio_set_level(TFT_DC, 1);
-        spi_transaction_t t_data = {
-            .length = param_size * 8,
-            .tx_buffer = param,
-        };
-        ret = spi_device_polling_transmit(spi, &t_data);
-        if (ret != ESP_OK) return -1;
-    }
-
-    return 0;
-}
-
-static void TestSenden(void *pvParameter)
-{
-    char daten[100]; 
-
-    esp_err_t ret;
-
-    while (1)
-    {
-        for ( int i = 0 ; i < 100 ; i++)
-        {
-            daten[i] = i; 
-        }
-        gpio_set_level(TFT_DC, 1);
-            spi_transaction_t t_data = {
-                .length = 100 * 8,
-                .tx_buffer = daten,
-            };
-            ret = spi_device_polling_transmit(spi, &t_data);
-            if (ret != ESP_OK)
-            {
-                ESP_LOGI("TestTask" , " Abbruch wegen fehler");
-                return ;
-        } 
-
-        vTaskDelay(pdMS_TO_TICKS(100));
-    }
 }
 
 // Task für LVGL
@@ -251,29 +191,24 @@ void app_main()
     // Erstellen des Displays 
     lv_lcd_flag_t flags = LV_LCD_FLAG_NONE;
     lv_display_t *disp = lv_st7735_create(EXAMPLE_LCD_H_RES, EXAMPLE_LCD_V_RES, flags, my_lcd_send_cmd, my_lcd_send_color);
-
-   // int32_t myComando( const uint8_t *cmd, size_t cmd_size, const uint8_t *param, size_t param_size)
    
     // Anlegen des Datenbuffers (Buffer nur 1/10 der Fläche)
     static uint8_t draw_buf1[128 * 160 / 10 * 2]; // 1/10 Fläche * 2 bytes per Pixel (RGB565)
     lv_display_set_buffers(disp, draw_buf1, NULL, sizeof(draw_buf1), LV_DISPLAY_RENDER_MODE_PARTIAL);
 
-    lv_display_set_color_format(disp, LV_COLOR_FORMAT_RGB565);
+   lv_display_set_color_format(disp, LV_COLOR_FORMAT_RGB565);
 
     lv_display_set_rotation(disp, LV_DISPLAY_ROTATION_90);
     lv_st7735_set_gap(disp, 0, 0); // falls nötig anpassen
 
     // Beispile inhalt erstellen 
-    lv_obj_set_style_bg_color(lv_screen_active(), lv_color_hex(0xff0000), LV_PART_MAIN);
+    lv_obj_set_style_bg_color(lv_screen_active(), lv_color_hex(0x000000), LV_PART_MAIN);
     //Create a white label, set its text and align it to the center
     lv_obj_t * label = lv_label_create(lv_screen_active());
     lv_label_set_text(label, "Hello world 1");
     lv_obj_set_style_text_color(lv_screen_active(), lv_color_hex(0xffffff), LV_PART_MAIN);
     lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
 
-
     // Start LVGL working Task 
     xTaskCreatePinnedToCore(lvgl_task, "lvgl", 4096, NULL, 1, NULL, 1);
-
-
 }
